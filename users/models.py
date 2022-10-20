d909ccf8d97291e28f3b185de6bc4750e99a39a6
msm9401 <anstmdals94@gmail.com>
2022-10-20 19:28:44 +0900

    create custom user models and admin

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -6,4 +8,7 @@ class User(AbstractUser):
 
     """model definition for users"""
 
-    pass
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    name = models.CharField(max_length=140, default="")
+    is_host = models.BooleanField(default=False)
