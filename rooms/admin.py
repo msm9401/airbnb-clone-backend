5f4dca36644811d4c12a1255d63740fef2b9e77c
msm9401 <anstmdals94@gmail.com>
2022-10-23 05:43:29 +0900

    rooms & amenities admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -4,9 +4,40 @@ from .models import Room, Amenity
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = (
+        "description",
+        "country",
+        "city",
+        "price",
+        "pet_friendly",
+        "kind",
+        "owner",
+        "created_at",
+        "updated_at",
+    )
+
+    list_filter = (
+        "country",
+        "city",
+        "pet_friendly",
+        "kind",
+        "created_at",
+        "updated_at",
+    )
 
 
 @admin.register(Amenity)
 class AmenityAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = (
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
