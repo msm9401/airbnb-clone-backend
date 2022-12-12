84ee45313b48582f5b83c7954c9bc2c98e2a7d59
msm9401 <anstmdals94@gmail.com>
2022-12-13 01:36:29 +0900

    Authentication

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "rest_framework.authtoken",
 ]
 
 CUSTOM_APPS = [
@@ -153,3 +154,12 @@ MEDIA_URL = "user-uploads/"
 
 
 PAGE_SIZE = 3
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        "config.authentication.JWTAuthentication",
+    ]
+}
